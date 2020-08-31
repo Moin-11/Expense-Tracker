91b5612fc09cc0248b94381f3326afb53018be2b
Moin-11 <Moin11.dev@gmail.com>
2020-08-31 22:58:00 +0530

    changed background

--- src/components/app.js
+++ src/components/app.js
@@ -6,28 +6,32 @@ import BalanceGrid from "../components/Balance_Grid";
 import TransactionHistoryList from "./transactionHistory";
 import { TransactionForm } from "../components/transactionForm";
 
-import background from "../images/pexels-miguel-á-padriñán-745365.jpg";
+import background from "../images/pexels-george-becker-374918.jpg";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const App = () => {
   return (
     <GlobalProvider>
-      <div
-        style={{
-          margin: "0px",
-          padding: "0px",
-          borderWidth: "0px",
-          backgroundImage: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center center",
-          backgroundRepeat: "no-repeat",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        <Header />
-        <BalanceGrid />
-        <TransactionHistoryList />
-        <TransactionForm />
-      </div>
+      <CssBaseline>
+        {" "}
+        <div
+          style={{
+            margin: "0px",
+            padding: "0px",
+            borderWidth: "0px",
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundAttachment: "fixed",
+          }}
+        >
+          <Header />
+          <BalanceGrid />
+          <TransactionHistoryList />
+          <TransactionForm />
+        </div>
+      </CssBaseline>
     </GlobalProvider>
   );
 };
