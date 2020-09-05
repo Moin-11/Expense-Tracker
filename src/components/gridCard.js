f90dc7ab161fd4eec8f8f95005a7c720c4607db7
Moin-11 <Moin11.dev@gmail.com>
2020-09-05 17:10:59 +0530

    another post-finish push

--- src/components/gridCard.js
+++ src/components/gridCard.js
@@ -7,7 +7,7 @@ const GridCard = ({ title, value }) => {
   const classes = GCardStyles();
 
   return (
-    <Grid item xs={4} md={4}>
+    <Grid item xs={11} md={4}>
       <Paper elevation={5} className={classes.paper}>
         <Typography variant="h1">{title}</Typography>
         <Typography variant="h2">$ {value}</Typography>
