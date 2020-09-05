827aa1b14c6d896103cc0d5e52a9646102532160
Moin-11 <Moin11.dev@gmail.com>
2020-09-05 17:16:50 +0530

    fixing makeStyles application

--- src/components/transactionForm.js
+++ src/components/transactionForm.js
@@ -1,10 +1,83 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context/globalContext";
 import { Title } from "./Title";
-import useStyles from "../styles/transactionFormStyles";
+import { makeStyles } from "@material-ui/core/styles";
 // UI Imports
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(() => ({
+  textFields: {
+    color: "white",
+    fontFamily: "Bebas Neue",
+    "&::placeholder": {
+      color: "lightgrey",
+      fontSize: "1.3rem",
+      opacity: "0.8",
+    },
+  },
+
+  root: {
+    display: "flex",
+    marginTop: "100px",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingLeft: "10px",
+
+    color: "white",
+    borderWidth: "3px",
+    borderColor: "white !important",
+    borderInlineColor: "white",
+    outlineColor: "white",
+
+    "& .MuiTextField-root": {
+      width: "50vw",
+    },
+    "& label.Mui-focused": {
+      color: "white",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "whiteSmoke",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderStyle: "solid",
+        borderWidth: "2px",
+        borderColor: "lightgrey",
+        color: "white",
+      },
+      "&:hover fieldset": {
+        borderColor: "white",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "white",
+        color: "white",
+      },
+      "&::placeholder": {
+        color: "white",
+      },
+
+      "& ::placeholder :hover": {
+        color: "white",
+      },
+    },
+  },
+  button: {
+    alignSelf: "center",
+    borderStyle: "solid",
+    borderWidth: "2.0px",
+    backgroundColor: "transparent",
+    color: "lightGrey",
+    fontFamily: "Bebas Neue",
+    fontSize: "1rem",
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "white",
+      opacity: "1.0",
+      transition: "0.3s",
+    },
+  },
+}));
 
 export const TransactionForm = () => {
   const classes = useStyles();
@@ -33,6 +106,7 @@ export const TransactionForm = () => {
   return (
     <form className={classes.root} onSubmit={onSubmit}>
       <Title titleText="Add New Transaction" />
+
       <div
         style={{
           padding: "0px 0px 0px 20px !important",
